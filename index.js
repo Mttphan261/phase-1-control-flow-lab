b4f7ab2d7d2af4d2fefa2774213e2ded8fce1751
Matt Phan <mttphan261@gmail.com>
2023-04-08 14:41:50 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(Feet){
+  if (Feet <= 400) {
+    return 'This one is on me!';
+  }
+  else if (Feet >= 2500) {
+    return 'No can do.'
+    }
+  else if (Feet >= 2000) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if  (Feet >= 400 &&  2000 ) {
+  return 'That will be twenty bucks.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(City){
+  return City === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
